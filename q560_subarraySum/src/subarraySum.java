ea48d7ce40c4e0a9144ea693c7780eb6cbf4b47d
oltreamre9 <1134625169@qq.com>
2019-12-11 21:11:22 +0800

    q560连续子数组和为k 蛮力法 新增hashmap记录每次累加和的数量，最后结果为键为sun-k的值数量

--- q560_subarraySum/src/subarraySum.java
+++ q560_subarraySum/src/subarraySum.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 /*
 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
 
@@ -15,18 +16,30 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int res=0;
-
+    //    public int subarraySum(int[] nums, int k) {
+//        int res=0;
+//
+//        for(int i=0;i<nums.length;i++){
+//            int sum=0;
+//            for(int j=i;j<nums.length;j++) {
+//                sum += nums[j];
+//                if (sum==k)
+//                    res++;
+//            }
+//        }
+//        return res;
+//    }
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum=0;
+        int count=0;
         for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++) {
-                sum += nums[j];
-                if (sum==k)
-                    res++;
-            }
+            sum+=nums[i];
+            if(map.containsKey(sum-k))
+                count+=map.get(sum-k);
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
-        return res;
+        return count;
     }
-
 }
