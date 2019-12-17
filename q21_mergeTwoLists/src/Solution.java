9758bf69957781d43c566e1443b7523ea6ad5880
oltreamre9 <1134625169@qq.com>
2019-12-17 14:18:17 +0800

    合并两个有序链表，操作类似于归并排序中的merge

---
+++ q21_mergeTwoLists/src/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res=new ListNode(-1);
+        ListNode temp=res;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                temp.next=l1;
+                l1=l1.next;
+            }else{
+                temp.next=l2;
+                l2=l2.next;
+            }
+            temp=temp.next;
+        }
+        while(l1!=null){
+            temp.next=l1;
+            l1=l1.next;
+            temp=temp.next;
+        }
+        while(l2!=null){
+            temp.next=l2;
+            l2=l2.next;
+            temp=temp.next;
+        }
+        return res.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
