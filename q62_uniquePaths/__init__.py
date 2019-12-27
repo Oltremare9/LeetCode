44fcb9cb7987ea0a4bb782141aab192b94fbbb09
oltreamre9 <1134625169@qq.com>
2019-12-27 16:54:13 +0800

    q62 只能右移和下移的路径数量

---
+++ q62_uniquePaths/__init__.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        matrix = [[1]*n for _ in range(m)]
+        print(matrix)
+        for i in range(1, m):
+            for j in range(1, n):
+                matrix[i][j]=matrix[i - 1][j] + matrix[i][j - 1]
+        return matrix[m - 1][n - 1]
